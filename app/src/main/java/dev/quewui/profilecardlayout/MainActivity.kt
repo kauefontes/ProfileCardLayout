4f43945fad0154e33ff5786df8737712bedc96e9
Kaue Pereira <kauefontes@outlook.com>
2022-10-04 23:14:51 -0400

    Handling large and dynamic lists with Coil

--- app/src/main/java/dev/quewui/profilecardlayout/MainActivity.kt
+++ app/src/main/java/dev/quewui/profilecardlayout/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,9 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import dev.quewui.profilecardlayout.ui.theme.DefaultTheme
 import dev.quewui.profilecardlayout.ui.theme.lightGreen
 
@@ -35,13 +36,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
-    Scaffold(topBar = { AppBar() }) { contentPadding ->
+    Scaffold(topBar = { AppBar() }) { padding ->
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
         ) {
-            Column {
-                for (userProfile in userProfiles)
+            LazyColumn() {
+                items(userProfiles) { userProfile ->
                     ProfileCard(userProfile)
+                }
             }
         }
     }
@@ -77,14 +81,14 @@ fun ProfileCard(userProfile: UserProfile) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture(userProfile.drawableId, userProfile.status)
+            ProfilePicture(userProfile.pictureUrl, userProfile.status)
             ProfileDetails(userProfile.name, userProfile.status)
         }
     }
 }
 
 @Composable
-fun ProfilePicture(drawableId: Int, status: Boolean) {
+fun ProfilePicture(pictureUrl: String, status: Boolean) {
     Card(
         shape = CircleShape,
         backgroundColor = Color.Black,
@@ -99,9 +103,9 @@ fun ProfilePicture(drawableId: Int, status: Boolean) {
         modifier = Modifier.padding(16.dp),
         elevation = 4.dp
     ) {
-        Image(
-            painter = painterResource(id = drawableId),
-            contentDescription = "Profile picture thumbnail",
+        AsyncImage(
+            model = (pictureUrl),
+            contentDescription = null,
             modifier = Modifier.size(72.dp),
             contentScale = ContentScale.Crop
         )
